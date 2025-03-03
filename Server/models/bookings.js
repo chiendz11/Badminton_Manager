ef208138c808e022796c30ad065284b14217a50b
chiendz11 <thuha25121976@gmail.com>
2025-03-03 15:08:51 +0700

    Update bookings.js

--- Server/models/bookings.js
+++ Server/models/bookings.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-<<<<<<< Updated upstream
+
   courtId: { type: mongoose.Schema.Types.ObjectId, ref: "Court", required: true },
   
   type: { type: String, enum: ["daily", "monthly"], required: true }, // daily hoặc monthly
